e573d271e513f6ac966ec893d00162cd0703f4dc
nancy <nancy65018@gmail.com>
2022-09-27 18:58:09 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const render = require("./router/render");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.use('/', render)
+
+const PORT = '3000';
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST);
+console.log('server listening at port 3000...');
