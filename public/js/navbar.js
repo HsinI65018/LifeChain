4993e0876f78959ef8751e94d647e7561d94e7c9
nancy <nancy65018@gmail.com>
2022-09-29 09:46:38 +0800

    feat: show wallet address after connected

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,10 @@
 const ethereumButton = document.querySelector('.navbar-connect-wallet-btn');
 const connectToWallet = async() => {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    console.log(accounts[0])
+    if(accounts[0].length !== 0){
+        ethereumButton.textContent = accounts[0].slice(0, 18) + '...';
+        console.log(accounts[0]);
+    }
 }
 ethereumButton.addEventListener('click', connectToWallet);
 
