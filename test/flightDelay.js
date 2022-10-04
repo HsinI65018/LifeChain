27e275aee164fac752c67c5d830b8c9d531dcb7b
nancy <nancy65018@gmail.com>
2022-10-04 20:21:45 +0800

    fix: update getInsurance fun by created getInsuranceIds, rewrite testing by chai

--- test/flightDelay.js
+++ test/flightDelay.js
@@ -1,25 +1,50 @@
 const flightDelay = artifacts.require("./flightDelay.sol");
+const expect = require("chai").expect;
 
 contract(flightDelay, async (accounts) => {
-    
-    it("it should create a new insurance data", async () => {
-        const flightInstance = await flightDelay.new();
 
+    let flightInstance;
+    beforeEach(async () => {
+        flightInstance = await flightDelay.new();
+    });
+    
+    it("it should add unique airline", async () => {
         const firstUser = accounts[0];
-        const secondUser = accounts[1];
-        const thirdUser = accounts[2];
         const airlineData = ["CI", "AI"];
 
         await flightInstance.createInsurance(firstUser, "CI", "113", "20201001");
-        await flightInstance.createInsurance(secondUser, "AI", "456", "20201002");
-        await flightInstance.createInsurance(thirdUser, "AI", "789", "20201002");
+        await flightInstance.createInsurance(firstUser, "AI", "456", "20201002");
+        await flightInstance.createInsurance(firstUser, "AI", "789", "20201002");
 
         const airlines = await flightInstance.getAirlines();
 
-        const userInsurance = await flightInstance.getInsurance(thirdUser);
-        console.log(userInsurance.airline)
-        console.log(userInsurance)
+        expect(airlines[0]).to.equal(airlineData[0]);
+        expect(airlines).to.have.lengthOf(2);
+    })
 
-        assert.equal(airlines[0], airlineData[0]);
+    it("it shoule create a new insurance data", async () => {
+        const firstUser = accounts[0];
+
+        await flightInstance.createInsurance(firstUser, "CI", "113", "20201001");
+        await flightInstance.createInsurance(firstUser, "AI", "456", "20201002");
+
+        const insuranceIds = await flightInstance.getInsuranceIds(firstUser);
+
+        expect(insuranceIds).to.have.lengthOf(2);
+    })
+
+    it("it should get insurance data by user id", async () => {
+        const firstUser = accounts[0];
+        const airlineData = ["CI", "AI"];
+
+        await flightInstance.createInsurance(firstUser, "CI", "113", "20201001");
+        await flightInstance.createInsurance(firstUser, "AI", "456", "20201002");
+
+        const insuranceIds = await flightInstance.getInsuranceIds(firstUser);
+
+        for(let i = 0; i < insuranceIds.length; i ++) {
+            const insuranceData = await flightInstance.getInsurance(insuranceIds[i]);
+            expect(insuranceData[0]).to.equal(airlineData[i]);
+        }
     })
 })
